d1a94666d29946241e574cf40a58022f5476f642
Ahmad Farisi <ahmfarisi@gmail.com>
2026-03-12 15:58:54 +0700

    Update

---
+++ Learn Javascript/about me/script.js
@@ -0,0 +1,12 @@
+function showProfile() {
+  document.getElementById("profile").innerHTML =
+    "Name: Ahmad Farisi <br> Age: 30 <br> Program: Informatics / Web Technology";
+}
+
+function changeBackground() {
+  document.body.style.backgroundColor = "#dbeafe";
+}
+
+function showWelcome() {
+  alert("Welcome to my page!");
+}
