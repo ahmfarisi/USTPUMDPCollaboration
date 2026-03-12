c6c7ee18f5c8e4458eb064de5438a98cc73daa67
Ahmad Farisi <ahmfarisi@gmail.com>
2026-03-12 14:47:11 +0700

    JS Project

---
+++ Learn Javascript/JS04/script.js
@@ -0,0 +1,7 @@
+let name = "Maria Santos";
+let age = 20;
+let program = "Information Technology";
+
+document.getElementById("name").innerHTML = "Name: " + name;
+document.getElementById("age").innerHTML = "Age: " + age;
+document.getElementById("program").innerHTML = "Program: " + program;
